9fd984c04d8fd6bbf40af0349bf8eb69a404ee2e
alnwangchi <alnwangchi@gmail.com>
2024-05-12 20:17:13 +0800

    feat: almost done 有處理精度問題 未處理例如按下運算符號先計算出結果顯示、重複按下運算符號情況等

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,41 +1,161 @@
 'use client';
-import React, { useState } from 'react';
-const regexIncludesNumbersCommas = /^(?:0|[1-9]\d*)(?:[.,]\d{0,18})?$|^$/g;
+import React, { useRef, useState } from 'react';
+import * as math from 'mathjs';
+
+const regex = /[0-9*+=/-]+/g;
+
 const Calculator = () => {
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState<number | string>('');
+  const operatorRef = useRef<{ prevNums: number[]; operator: string[] }>({
+    prevNums: [],
+    operator: [],
+  });
+
+  const handleNumber = (num: number) => {
+    setNumber((prev) => {
+      if (prev === 0) return num.toString();
+      return prev + num.toString();
+    });
+  };
+
+  // 紀錄數字與運算符號
+  const handleOperator = (operator: string) => {
+    operatorRef.current.prevNums = [...operatorRef.current.prevNums, +number];
+    operatorRef.current.operator = [...operatorRef.current.operator, operator];
+    setNumber(0);
+  };
+
+  const handleResult = () => {
+    // 加上最後 input 上的數字
+    operatorRef.current.prevNums = [...operatorRef.current.prevNums, +number];
+    // 將 prevNums 與 operator 進行運算
+    const result = operatorRef.current.prevNums.reduce((acc, num, idx) => {
+      switch (operatorRef.current.operator[idx - 1]) {
+        case '+':
+          return +math.add(math.bignumber(acc), math.bignumber(num));
+        case '-':
+          return +math.subtract(math.bignumber(acc), math.bignumber(num));
+        case '*':
+          return +math.multiply(math.bignumber(acc), math.bignumber(num));
+        case '/':
+          return +math.divide(math.bignumber(acc), math.bignumber(num));
+        default:
+          return num;
+      }
+    });
+
+    operatorRef.current.prevNums = [];
+    operatorRef.current.operator = [];
+    setNumber(result.toString());
+  };
 
   return (
     <div className='flex flex-col gap-5'>
       <h1>React TS Calculator</h1>
-      <div className=''>
+      <div>
         <input
           className='rounded-md text-right w-full h-[72px] px-5 bg-[#232c43] text-2xl '
           type='text'
-          value={number}
+          // readonly
+          value={number || 0}
           onChange={(e) => {
-            setNumber(Number(e.target.value));
+            if (regex.test(e.target.value)) {
+              console.log(e.target.value);
+            }
           }}
         />
       </div>
       <div className='grid grid-areas-layout grid-cols-layout grid-rows-layout gap-3 bg-[#232c43] p-5 rounded-md'>
-        <button className='common-btn grid-in-cancel'>CAN</button>
-        <button className='common-btn grid-in-del'>DEL</button>
-        <button className='common-btn grid-in-divide'>/</button>
-        <button className='common-btn grid-in-mult'>x</button>
-        <button className='common-btn grid-in-seven'>7</button>
-        <button className='common-btn grid-in-eight'>8</button>
-        <button className='common-btn grid-in-nine'>9</button>
-        <button className='common-btn grid-in-minus'>-</button>
-        <button className='common-btn grid-in-four'>4</button>
-        <button className='common-btn grid-in-five'>5</button>
-        <button className='common-btn grid-in-six'>6</button>
-        <button className='common-btn grid-in-add'>+</button>
-        <button className='common-btn grid-in-one'>1</button>
-        <button className='common-btn grid-in-two'>2</button>
-        <button className='common-btn grid-in-three'>3</button>
-        <button className='common-btn grid-in-zero'>0</button>
-        <button className='common-btn grid-in-dot'>.</button>
-        <button className='common-btn grid-in-equal'>=</button>
+        {/* numbers */}
+        <button onClick={() => handleNumber(1)} className='common-btn grid-in-one'>
+          1
+        </button>
+        <button onClick={() => handleNumber(2)} className='common-btn grid-in-two'>
+          2
+        </button>
+        <button onClick={() => handleNumber(3)} className='common-btn grid-in-three'>
+          3
+        </button>
+        <button onClick={() => handleNumber(4)} className='common-btn grid-in-four'>
+          4
+        </button>
+        <button onClick={() => handleNumber(5)} className='common-btn grid-in-five'>
+          5
+        </button>
+        <button onClick={() => handleNumber(6)} className='common-btn grid-in-six'>
+          6
+        </button>
+        <button onClick={() => handleNumber(7)} className='common-btn grid-in-seven'>
+          7
+        </button>
+        <button onClick={() => handleNumber(8)} className='common-btn grid-in-eight'>
+          8
+        </button>
+        <button onClick={() => handleNumber(9)} className='common-btn grid-in-nine'>
+          9
+        </button>
+        <button onClick={() => handleNumber(0)} className='common-btn grid-in-zero'>
+          0
+        </button>
+        <button
+          onClick={() => {
+            setNumber((prev) => {
+              if (prev.toString().includes('.')) return prev;
+              return prev + '.';
+            });
+          }}
+          className='common-btn grid-in-dot'
+        >
+          .
+        </button>
+
+        {/* operators */}
+        <button
+          onClick={() => {
+            handleOperator('/');
+          }}
+          className='common-btn grid-in-divide'
+        >
+          /
+        </button>
+        <button
+          onClick={() => {
+            handleOperator('*');
+          }}
+          className='common-btn grid-in-mult'
+        >
+          x
+        </button>
+        <button
+          onClick={() => {
+            handleOperator('-');
+          }}
+          className='common-btn grid-in-minus'
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            handleOperator('+');
+          }}
+          className='common-btn grid-in-add'
+        >
+          +
+        </button>
+
+        {/* others */}
+        <button onClick={() => setNumber(0)} className='common-btn grid-in-cancel'>
+          CAN
+        </button>
+        <button
+          onClick={() => setNumber((prev) => prev.toString().slice(0, -1))}
+          className='common-btn grid-in-del'
+        >
+          DEL
+        </button>
+        <button onClick={handleResult} className='common-btn grid-in-equal'>
+          =
+        </button>
       </div>
     </div>
   );
